0c70d1ae09cd97b09421b147e1b703637035ed4a
SeddikSalma <seddiksalma@gmail.com>
2022-12-19 00:02:44 +0100

    first commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TP4.Data;
+using TP4.Models;
+
+namespace TP4.Controllers
+{
+    public class CourseController : Controller
+    {
+        // GET: Course
+        //get all courses
+        public ActionResult Index()
+        {
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            //Debug.WriteLine("Hello");
+            StudentRepository studentRepository = new StudentRepository(universityContext);
+           // Debug.WriteLine("Hello2");
+           // Debug.WriteLine(studentRepository);
+           
+
+            return View(studentRepository.GetCourses());
+        }
+
+        //get students of specific course
+        public ActionResult GetStudentsByCourse(string id)
+        {
+            UniversityContext universityContext = UniversityContext.Instantiate_UniversityContext();
+            StudentRepository studentRepository = new StudentRepository(universityContext);
+            IEnumerable<Student> stud = (IEnumerable<Student>)studentRepository.Find(v => v.course.ToLower() == id.ToLower());         
+            if (stud.Count() != 0) ViewBag.Id = stud.First().course;
+            return View(stud);
+        }
+    }
+}
---
+++ Data/UniversityContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using TP4.Models;
+
+namespace TP4.Data
+{
+    public class UniversityContext : DbContext
+    {
+        public DbSet<Student> Student { get; set; }
+        static private UniversityContext universityContextInstance = null;
+        static public int count = 0;
+        private UniversityContext(DbContextOptions o) : base(o)
+        {
+
+            count++;
+            Debug.WriteLine("instance :", count);
+        }
+
+        static public UniversityContext Instantiate_UniversityContext()
+        {
+            if (universityContextInstance == null)
+            {
+
+                var optionBuilder = new DbContextOptionsBuilder<UniversityContext>();
+                Debug.WriteLine("1 ");
+                optionBuilder.UseSqlite(@"Data Source=C:\Users\Salma\Downloads\db.db;");
+                Debug.WriteLine("2");
+                universityContextInstance = new UniversityContext(optionBuilder.Options);
+                Debug.WriteLine("3");
+                return universityContextInstance;
+            }
+            else
+            {
+                return universityContextInstance;
+            }
+        }
+    }
+}
